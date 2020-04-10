fad20cd369d4325e9a9848378a3f622565fc0329
Timon Bestebreur <timonbestebreur@gmail.com>
2020-04-10 23:21:41 +0200

    Final working version with lots of output :D

--- main.c
+++ main.c
@@ -13,19 +13,37 @@ int main () {
         printf("File loading went wrong!");
     }
 
-    printf("%s\n", buf);
 
-    // Get a 10-digit part of e, starting at index 2
-    char slice[12];
-    sprintf(slice, "%.10s\n", buf+2);
+    int maxLoop = 1002 - 10 - 1; // the amount of loops that can be done with 1002 chars
+    char slice[12]; // placeholder to hold the relevant chars that will be converted to a long, which then will be checked on being prime or not
+    char *ptr; // return value for sprintf
+    long candidate; // variable that holds potential prime number. The sieve will be called on this number.
 
-    printf("slice: %s\n", slice);
-    char *ptr;
-    long candidate;
-    
-    candidate = strtol(slice, &ptr, 10);
-    printf("candidate: %lu\n", candidate);
+    // loop over the digits of E, getting them in slices of 10
+    for (int i = 0; i < maxLoop; i++)
+    {
+        sprintf(slice, "%.10s\n", buf + 2 + i); // get a slice of 10 digits from the file (from where we left off)
+        candidate = strtol(slice, &ptr, 10); // convert this slice to a long
+        // printf("candidate %d: %ld\n", i, candidate);
+        printf("Testing the following candidate on prime-ness: %ld\n", candidate);
+        if(isPrime(candidate) == 1) {
+            printf("THE PRIME IS: %ld\n", candidate);
+            return 0;
+        }
+    }
 
     fclose(file);
     return 0;
+}
+
+// calculates whether or not the given long is prime
+// returns 1 if the given long is prime, 0 if not
+int isPrime (long candidate) {
+    for (long i = 2; i < candidate; i++)
+    {
+        if(candidate % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
 }
